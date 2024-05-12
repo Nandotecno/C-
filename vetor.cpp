be03a2a7c13e9a8bfec0005df3bc32b8cabc2023
Fernando Cruz <86312129+Nandotecno@users.noreply.github.com>
2024-05-12 18:38:14 -0300

    Add files via upload

---
+++ vetor.cpp
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main(){
+	float notas [3];
+	
+	printf("Nota 1: ");
+	scanf("%f", &notas[0]);
+	
+	printf("Nota 2: ");
+	scanf("%f", &notas[1]);
+	
+	printf("Nota 3: ");
+	scanf("%f", &notas[2]);
+	
+	printf("Notas: \n");
+	printf(" - %.1f \n", notas[0]);
+	printf(" - %.1f \n", notas[1]);
+	printf(" - %.1f \n", notas[2]);
+	
+	return 0;
+	
+	
+}
